9122fe506758f3baf34245a34c7f5291269a7026
CffAyy <49475686+CffAyy@users.noreply.github.com>
2019-04-22 08:19:34 +0800

    Update VerificationCode.java

--- VerificationCode.java
+++ VerificationCode.java
@@ -15,11 +15,11 @@ import cn.itcast.vcode.utils.VerifyCode;
 public class VerificationCode {
 	@Test
 	public void fun1() throws FileNotFoundException, IOException {
-		VerifyCode verifyCode=new VerifyCode();
-		BufferedImage bi=verifyCode.getImage();
+		VerifyCode verifyCode=new VerifyCode();			//验证码
+		BufferedImage bi=verifyCode.getImage();			//创建指定大小、指定图像类型的BufferedImage对象。
 		System.out.println((verifyCode).getText());
 		
-		VerifyCode.output(bi, new FileOutputStream("E:/java web/classwork/WebContent/img/a.jpg"));
+		VerifyCode.output(bi, new FileOutputStream("E:/java web/classwork/WebContent/img/a.jpg"));	//存放位置
 	}
 
 	}
